fe35e1e13b6e70fa39ed43664adaeec1e2c0e43a
vbustamante <victorbustamante12@gmail.com>
2019-10-10 16:39:24 -0300

    Added sample data stubs and single select example

--- examples/init_gh.rs
+++ examples/init_gh.rs
@@ -1,5 +1,7 @@
 use cabide::Cabide;
+use csv::Reader;
 use serde::{Deserialize, Serialize};
+use std::fs::File;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 struct Data {
@@ -10,19 +12,17 @@ struct Data {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Truncates file to clear it for this run
+    File::create("select.file")?;
 
     let mut cbd: Cabide<Data> = Cabide::new("select.file", None)?;
 
-    let result = &cbd.read(0);
-
-    match  result {
-        Ok(_v) => println!("Found {} from {}", _v.uhe, _v.estagio),
-        Err(_e) => println!("Found nothing"),
+    let mut csv = Reader::from_reader(File::open("data/gh_head.csv")?);
+    for data in csv.deserialize() {
+        let data = data?;
+        let block = cbd.write(&data)?;
+        assert_eq!(cbd.read(block)?, data);
     }
-
-    println!();
-    println!("used blocks: {}", 1);
-    println!("read blocks: {}", 1);
 
     Ok(())
 }
