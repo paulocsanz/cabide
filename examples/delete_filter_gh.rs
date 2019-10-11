365929e78fbcbeb11d2296b8488d99da65766070
joaohenriquef <joaohenriquef@poli.ufrj.br>
2019-10-10 22:55:44 -0300

    Add delete and delete_filter examples

---
+++ examples/delete_filter_gh.rs
@@ -0,0 +1,29 @@
+use cabide::Cabide;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct Data {
+    uhe: u64,
+    cenario: u64,
+    estagio: String,
+    geracao: f64,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cbd: Cabide<Data> = Cabide::new("select.file", None)?;
+
+    // Edit function passed to filter to change select condition
+    let results = cbd.remove_with(|student| student.estagio == "2017-01-01");
+
+    for result in results.iter() {
+        println!("Found {} from {}", result.uhe, result.estagio);
+    }
+
+
+    // TODO op reporting
+    println!();
+    println!("used blocks: {}", cbd.blocks()?);
+    println!("read blocks: {}", 1);
+
+    Ok(())
+}
